7e4451531b20200591118354060c9077faa799c2
Feterandjr <71264432+Feterandjr@users.noreply.github.com>
2020-10-17 13:05:38 +0200

    Add files via upload

---
+++ multiplicacion.py
@@ -0,0 +1,11 @@
+'''Escriba una función llamada “multiplicación”
+que reciba como argumento cuatro números reales
+distintos de cero y que devuelva su producto.'''
+def multiplicacion():
+    first=int(input("First Number:"))
+    second=int(input("Second Number:"))
+    third=int(input("Third Number:"))
+    fourth=int(input("Fourth Number:"))
+    print ("Answer is",first*second*third*fourth)
+
+multiplicacion()
